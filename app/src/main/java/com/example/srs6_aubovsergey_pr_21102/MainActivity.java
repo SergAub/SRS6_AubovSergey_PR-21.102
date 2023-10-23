99496ec1b1ce53cf70ff72ca6e59eabd38718dae
Sergey <sergeypanz355@mail.ru>
2023-10-24 01:20:25 +0700

    Проект создан

---
+++ app/src/main/java/com/example/srs6_aubovsergey_pr_21102/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.srs6_aubovsergey_pr_21102;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SRS6_AubovSergey_PR-21.102"
+include(":app")
