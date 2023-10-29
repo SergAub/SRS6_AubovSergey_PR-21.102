d1dc803ad443088786623258289a5d28088322d4
Sergey <sergeypanz355@mail.ru>
2023-10-29 18:52:38 +0700

    Готово

---
+++ app/src/main/java/com/example/srs6_aubovsergey_pr_21102/Weather.java
@@ -0,0 +1,30 @@
+package com.example.srs6_aubovsergey_pr_21102;
+
+public class Weather {
+    String date;
+    String temp;
+    String we;
+    public Weather(String date, String temp, String we)
+    {
+        this.date= date;
+        this.temp= temp;
+        this.we= we;
+    }
+    public String GetDate()
+    {
+        return this.date;
+    }
+    public void SetDate(String date)
+    {
+        this.date= date;
+    }
+    public String GetUrl()
+    {
+        return this.we;
+    }
+    public String GetTemp()
+    {
+        return this.temp;
+    }
+
+}
